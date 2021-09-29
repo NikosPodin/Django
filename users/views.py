6d8ef0a408a2f52ea14ebfe9dabbfd1aa1ba6722
My GitHub Usernmaen <nlp17@mail.ru>
2021-09-29 23:38:54 +0500

    added register & login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    contex ={
+        'title': 'Geekshop - Авторизация'
+     }
+    return render(request,'users/login.html', contex)
+
+def register(request):
+    contex: {
+        'title': 'Geekshop - Регистрация'
+    }
+    return render(request,'users/register.html', contex)
