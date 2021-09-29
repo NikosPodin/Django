89a57da7379e6f8baa3538c7f0ec6c6d46d989ba
My GitHub Usernmaen <nlp17@mail.ru>
2021-09-30 02:16:50 +0500

    work with auth login

--- users/views.py
+++ users/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
+
 
 # Create your views here.
 def login(request):
-    contex ={
-        'title': 'Geekshop - Авторизация'
-     }
-    return render(request,'users/login.html', contex)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Geekshop - Авторизация',
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
+
 
 def register(request):
-    contex: {
-        'title': 'Geekshop - Регистрация'
+    context = {
+        'title': 'Geekshop - Регистрация',
     }
-    return render(request,'users/register.html', contex)
+    return render(request, 'users/register.html', context)
