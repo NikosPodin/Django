26b85c07b9773ba70702ff209cdcdbdfea5803fd
My GitHub Usernmaen <nlp17@mail.ru>
2021-10-14 00:21:17 +0500

    add @login_required, total_sum,() & total_quantity() is done

--- baskets/urls.py
+++ baskets/urls.py
@@ -22,4 +22,5 @@ app_name = 'baskets'
 urlpatterns = [
     path('add/<int:product_id>', baskets.basket_add, name='basket'),
     path('remove/<int:product_id>', baskets.basket_remove, name='basket_remove'),
+    path('edit/<int:id>/<int:quantity>/',baskets.basket_edit, name='basket_edit')
 ]
--- baskets/views.py
+++ baskets/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 from mainapp.models import Product
 from baskets.models import Basket
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 
 @login_required
@@ -25,3 +27,20 @@ def basket_add(request, product_id):
 def basket_remove(request, product_id):
     Basket.objects.get(id=product_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required
+def basket_edit(request, id, quantity):
+     if request.is_ajax():
+         basket = Basket.objects.get(id=id)
+         if quantity > 0:
+             basket.quantity = quantity
+             basket.save()
+         else:
+             basket.delete()
+
+         baskets = Basket.objects.filter(user = request.user)
+         context = {
+             'baskets':baskets
+         }
+         result = render_to_string('baskets.html', context)
+         return JsonResponse({'result':result})
